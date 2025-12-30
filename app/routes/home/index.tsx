490202f7965eeb1c099ddb6df68818642572c276
Irene-IT <irene.it.work@gmail.com>
2025-12-30 18:19:29 +0100

    upd home

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -2,7 +2,8 @@
 // import Hero from '~/components/Hero';
 
 import type { Route } from './+types/index';
-import type { Project } from '~/types';
+// import type { Project } from '~/types';
+import type { Project, StrapiProject, StrapiResponse } from '~/types';
 import FeaturedProjects from '~/components/featured-projects';
 import AboutPreview from '~/components/about-preview';
 import type { PostMeta } from '~/types';
@@ -20,15 +21,35 @@ export async function loader({
     fetch(new URL('/data/posts-meta.json', url)),
   ]);
 
-  if (!projectRes.ok || !postRes.ok) {
+if (!projectRes.ok || !postRes.ok) {
     throw new Error('Не вдалося завантажити проєкти або пости');
   }
 
-  const [projects, posts] = await Promise.all([
-    projectRes.json(),
-    postRes.json(),
-  ]);
+  // const [projects, posts] = await Promise.all([
+  //   projectRes.json(),
+  //   postRes.json(),
+  // ]);
 
+  // Парсимо JSON
+  const posts: PostMeta[] = await postRes.json();
+
+  // Нормалізуємо projects як масив Project
+  const json: StrapiResponse<StrapiProject> = await projectRes.json();
+ 
+   const projects = json.data.map((item) => ({
+     id: item.id,
+     documentId: item.documentId,
+     title: item.title,
+     description: item.description,
+     image: item.image?.url
+       ? `${item.image.url}`
+       : '/images/no-image.png',
+     url: item.url,
+     date: item.date,
+     category: item.category,
+     featured: item.featured,
+   }));
+  
   return { projects, posts };
 }
 
